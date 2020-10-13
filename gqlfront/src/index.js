5c9f6d6fc0b3ef8426b46880c037e1cbb649ed19
handaad <aakash.handa@natwest.com>
2020-10-13 22:55:37 +0530

    a

---
+++ gqlfront/src/index.js
@@ -0,0 +1,49 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+import {Query} from 'react-apollo';
+
+const client = new ApolloClient({
+    uri:'http://localhost:8600/graphql'
+})
+
+const GET_PRODUCTS=gql`
+    query product($id:Int){
+        product(id:$id){
+            name,
+            rating_text
+        }
+    }
+`
+
+class ProductDetails extends Component{
+    constructor(){
+        super()
+
+        this.state={
+            id:4
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <Query query={GET_PRODUCTS} client={client} variables={{id:this.state.id}}>
+                    {({loading,error,data}) => {
+                        if(loading) return <p>Loading...</p>
+                        if(error) return <p>Error...</p>
+                        return(
+                            <div>
+                                <h2>{data.product.name}</h2>
+                                <h2>{data.product.rating_text}</h2>
+                            </div>
+                        )
+                    }}
+                </Query>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<ProductDetails/>,document.getElementById('root'))
